8e55e9fd0d72ddfbda7baf255b08c3408ee04356
brokelynn <108573568+brokelynn@users.noreply.github.com>
2022-07-05 04:56:54 +0700

    fixed

--- autoexecute.lua
+++ autoexecute.lua
@@ -9,7 +9,7 @@ local scriptUrl = ({
     [286090429] = "https://raw.githubusercontent.com/RandomAdamYT/DarkHub_V4/main/init",
     [292439477] = "https://raw.githubusercontent.com/VoidMasterX/strawhook/main/script.lua",
     [1962086868] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Tower%20of%20Hell/Tower%20of%20Hell",
-    [3461767162]  = "https://raw.githubusercontent.com/GoodB0y08/Sk1dW6r3.lua/main/init.lua"
+    [3461767162]  = "https://raw.githubusercontent.com/GoodB0y08/Sk1dW6r3.lua/main/init.lua",
     [5307215810]  = "https://raw.githubusercontent.com/brokelynn/autoexec-scripts/main/games/randomizer/hi%20.lua"
 
  })[game.PlaceId]
