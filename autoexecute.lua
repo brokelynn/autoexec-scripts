5cdde9ebfc143dd4cb311dbcb2ed97d5df4bd39e
brokelynn <108573568+brokelynn@users.noreply.github.com>
2022-07-05 05:00:49 +0700

    actually fix this time

--- autoexecute.lua
+++ autoexecute.lua
@@ -1,6 +1,9 @@
+
 repeat wait() until Game:IsLoaded()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-
+if PlaceId == 6293968958 or 6853485271 then 
+	game.Players.LocalPlayer:Kick("games with strict anticheat detected! please disable auto-execute or dont inject your executor!")
+end
 local scriptUrl = ({
     [9611595239] = "https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua",
     [6872265039] = "https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua",
@@ -9,11 +12,9 @@ local scriptUrl = ({
     [286090429] = "https://raw.githubusercontent.com/RandomAdamYT/DarkHub_V4/main/init",
     [292439477] = "https://raw.githubusercontent.com/VoidMasterX/strawhook/main/script.lua",
     [1962086868] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Tower%20of%20Hell/Tower%20of%20Hell",
-    [3461767162]  = "https://raw.githubusercontent.com/GoodB0y08/Sk1dW6r3.lua/main/init.lua",
-    [5307215810]  = "https://raw.githubusercontent.com/brokelynn/autoexec-scripts/main/games/randomizer/hi%20.lua"
+    [3461767162]  = "https://raw.githubusercontent.com/GoodB0y08/Sk1dW6r3.lua/main/init.lua"
 
  })[game.PlaceId]
-
 
 
 if scriptUrl then
@@ -25,9 +26,7 @@ if scriptUrl then
 })
 wait(10)	
  loadstring(game:HttpGet(scriptUrl))()
-   else if PlaceId == 6293968958 or 6853485271 then 
-	game.Players.LocalPlayer:Kick("games with strict anticheat detected! please disable auto-execute or dont inject your executor!")
-   else
+else
 	OrionLib:MakeNotification({
 	Name = "Script autoexec",
 	Content = "game not supported! halting auto-execution",
