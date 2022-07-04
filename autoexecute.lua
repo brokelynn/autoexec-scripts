f2fd10cf9f09c330bbf292c8d633b22f019c72d0
brokelynn <108573568+brokelynn@users.noreply.github.com>
2022-07-05 00:07:19 +0700

    minor stuffs

--- autoexecute.lua
+++ autoexecute.lua
@@ -10,8 +10,10 @@ local scriptUrl = ({
     [292439477] = "https://raw.githubusercontent.com/VoidMasterX/strawhook/main/script.lua",
     [1962086868] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Tower%20of%20Hell/Tower%20of%20Hell",
     [3461767162]  = "https://raw.githubusercontent.com/GoodB0y08/Sk1dW6r3.lua/main/init.lua"
+    [5307215810]  = "https://raw.githubusercontent.com/brokelynn/autoexec-scripts/main/games/randomizer/hi%20.lua"
 
  })[game.PlaceId]
+
 
 
 if scriptUrl then
@@ -23,8 +25,10 @@ if scriptUrl then
 })
 wait(10)	
  loadstring(game:HttpGet(scriptUrl))()
+   else if PlaceId == 6293968958 or 6853485271 then 
+	game.Players.LocalPlayer:Kick("games with strict anticheat detected! please disable auto-execute or dont inject your executor!")
    else
-OrionLib:MakeNotification({
+	OrionLib:MakeNotification({
 	Name = "Script autoexec",
 	Content = "game not supported! halting auto-execution",
 	Image = "rbxassetid://9524079134",
