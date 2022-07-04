ba807aefdc8b913638e4648bca43e3834f4a1dd2
brokelynn <108573568+brokelynn@users.noreply.github.com>
2022-07-04 20:49:45 +0700

    Create pf only.lua

---
+++ pf only.lua
@@ -0,0 +1,15 @@
+repeat wait() until Game:IsLoaded()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+if game.PlaceId == 292439477 then  
+  OrionLib:MakeNotification({
+    Name = "PHANTOM FORCES IS GAY",
+    Content = "hi you played pahtom froces now loading script (15 seconds)",
+    Time = 15
+  })
+  wait(15)
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/VoidMasterX/strawhook/main/script.lua", true))()
+   else
+game.Players.LocalPlayer:Kick("pls payl pahntom focres  (teleporting to phantom forces in 5 seconds)")
+wait(5)
+game:GetService("TeleportService"):Teleport(292439477, LocalPlayer) 
+end
